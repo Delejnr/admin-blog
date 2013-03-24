04c68e055cc88abeccf5f01e29858542fa6ff9e4
muntwysm <stefan.muntwyler@gmail.com>
2013-03-24 21:48:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdminCrud::Application.config.secret_token = '994ac8aba55af725989cfc530b9742c12bee9fa132405594e25409254c783662da7947d0c34f957930290624f5c5c74e894c227926f87aef7550f6bdd28ca5c9'
