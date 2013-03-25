c38e163807b993502e6c11baaf25f931fbe10bea
muntwysm <stefan.muntwyler@gmail.com>
2013-03-25 14:00:19 +0100

    final touches

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,4 @@
 class Admin::AdminController < ApplicationController
-  http_basic_authenticate_with name: "admin", password: "foobar"
+  http_basic_authenticate_with name: "admins", password: "foobar"
+
 end
